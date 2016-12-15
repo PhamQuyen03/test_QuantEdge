a377984269ccca5486b51a14eb98ab33961d542c
PhamQuyen03 <phamthequyen03@gmail.com>
2016-12-15 08:51:56 +0700

    push 15-12

---
+++ VND/Scripts/controller.js
@@ -0,0 +1,28 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('controllerTest', function ($scope, $http, $location) {
+            var apiUrl = 'http://api.fixer.io/latest?base=USD';
+            var apiUrl1 = 'http://www.apilayer.net/api/live?access_key=c537cb0d3d13db7612ff7de2c71c0acf&format=1';
+            $http.get(apiUrl)
+                .then(function(response) {
+                    $scope.myWelcome = response.data;
+                    var listKey = response.data.rates;
+                    var keys = Object.keys(listKey);
+                    $scope.sizeList = keys.length;
+                    $scope.usd_eur = keys;
+                    var list = [];
+                    var i;
+                    for (i = 0; i < keys.length; ++i)
+                    {
+                        list.push(listKey[keys[i]]);
+                    }
+                    $scope.sells = list;
+                    
+                });
+
+        });
+    
+})();
---
+++ VND/Scripts/vnd.js
@@ -0,0 +1,54 @@
+﻿$(document).ready(function () {
+    var myTimer = setInterval(random, 800);
+    function random() {
+        var lengthArr = document.getElementById('sizeList').textContent;
+
+        for (var i = 0; i < lengthArr; i++) {
+            var num = Math.floor(Math.random() * (50 - (-50) + 1) + (-50));
+            var num1 = Math.floor(Math.random() * (50 - (-50) + 1) + (-50));
+            var sell = document.getElementById("sell" + i.toString()).textContent;
+            var valueSell = parseFloat(sell).toFixed(4);
+            var buy = document.getElementById("buy" + i.toString()).textContent;
+            var high = parseFloat(document.getElementById("high" + i.toString()).textContent);
+            var low = parseFloat(document.getElementById("low" + i.toString()).textContent);
+
+            var rate = parseFloat(sell) * (num / 1000);
+            var rate1 = parseFloat(buy) * (num1 / 1000);
+            var sum = parseFloat(sell) + rate;
+
+            var sum1 = parseFloat(buy) + rate1;
+            if (sum1 > sum) {
+                document.getElementById("sell" + i.toString()).innerHTML = sum.toFixed(4);
+                document.getElementById("buy" + i.toString()).innerHTML = sum1.toFixed(4);
+                document.getElementById("change" + i.toString()).innerHTML = num / 1000;
+                if (num > 0) {
+                    document.getElementById("sell" + i.toString()).style.color = "blue";
+                    document.getElementById("buy" + i.toString()).style.color = "blue";
+                    document.getElementById("caret" + i.toString()).style.color = "blue";
+                    $("#caret" + i.toString()).attr("class", "fa fa-caret-up");
+                }
+                else if (num < 0) {
+                    document.getElementById("sell" + i.toString()).style.color = "red";
+                    document.getElementById("buy" + i.toString()).style.color = "red";
+                    document.getElementById("caret" + i.toString()).style.color = "red";
+                    $("#caret" + i.toString()).attr("class", "fa fa-caret-down");
+                    
+                }
+                else {
+                    document.getElementById("sell" + i.toString()).style.color = "black";
+                    document.getElementById("buy" + i.toString()).style.color = "black";
+                }
+                if (sum.toFixed(4) * 10000 < low.toFixed(4) * 10000) {
+                    document.getElementById("low" + i.toString()).innerHTML = sum.toFixed(4);
+                }
+                if (sum.toFixed(4) * 10000 > high.toFixed(4) * 10000) {
+                    document.getElementById("high" + i.toString()).innerHTML = sum.toFixed(4);
+                }
+            }
+        }
+
+
+
+        //}
+    }
+});
