04f4152c120a4308a88617d465b044fe2c497d68
PhamQuyen03 <phamthequyen03@gmail.com>
2016-12-15 18:35:13 +0700

    no message

--- VND/Scripts/vnd.js
+++ VND/Scripts/vnd.js
@@ -4,34 +4,30 @@
         var lengthArr = document.getElementById('sizeList').textContent;
 
         for (var i = 0; i < lengthArr; i++) {
-            var num = Math.floor(Math.random() * (30 - (-30) + 1) + (-30));
-            var num1 = Math.floor(Math.random() * (30 - (-30) + 1) + (-30));
+            var randomSell = Math.floor(Math.random() * (30 - (-30) + 1) + (-30));
+            var randomBuy = Math.floor(Math.random() * (30 - (-30) + 1) + (-30));
             var sell = document.getElementById("sell" + i.toString()).textContent;
             var buy = document.getElementById("buy" + i.toString()).textContent;
             var high = parseFloat(document.getElementById("high" + i.toString()).textContent);
             var low = parseFloat(document.getElementById("low" + i.toString()).textContent);
 
-            var rate = parseFloat(sell) * (num / 1000);
-            var rate1 = parseFloat(buy) * (num1 / 1000);
-            var sum = parseFloat(sell) + rate;
-            var sum1 = parseFloat(buy) + rate1;
-            if (num < 0 && num1 > 0 || num > 0 && num1 > 0)
-            {
-
-            }
-            else
+            var rateSell = parseFloat(sell) * (randomSell / 1000);
+            var rateBuy = parseFloat(buy) * (randomBuy / 1000);
+            var sum = parseFloat(sell) + rateSell;
+            var sum1 = parseFloat(buy) + rateBuy;
+            if (randomSell < 0 && randomBuy < 0 || randomSell > 0 && randomBuy > 0)
             {
                 if (sum1 < sum) {
                     document.getElementById("sell" + i.toString()).innerHTML = sum.toFixed(4);
                     document.getElementById("buy" + i.toString()).innerHTML = sum1.toFixed(4);
-                    document.getElementById("change" + i.toString()).innerHTML = num / 1000;
-                    if (num > 0) {
+                    document.getElementById("change" + i.toString()).innerHTML = randomSell / 1000;
+                    if (randomSell > 0) {
                         document.getElementById("sell" + i.toString()).style.color = "blue";
                         document.getElementById("buy" + i.toString()).style.color = "blue";
                         document.getElementById("caret" + i.toString()).style.color = "blue";
                         $("#caret" + i.toString()).attr("class", "fa fa-caret-up");
                     }
-                    else if (num < 0) {
+                    else if (randomSell < 0) {
                         document.getElementById("sell" + i.toString()).style.color = "red";
                         document.getElementById("buy" + i.toString()).style.color = "red";
                         document.getElementById("caret" + i.toString()).style.color = "red";
